261b2bd0eab0736062a80b8e640537c55acb5772
blightfoot71 <brianlightfoot71@gmail.com>
2020-04-23 19:01:53 -0500

    Section 4 Module 3

--- Angular/Angular-Fundementals/ng-fundamentals/src/app/events/events-list.component.ts
+++ Angular/Angular-Fundementals/ng-fundamentals/src/app/events/events-list.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-list',
-  templateUrl: './events-list.component.html'
+  template: `
+  <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr/>
+    <events-thumbnail [event]="event1"></events-thumbnail>
+  </div>
+  `
 })
 export class EventsListComponent {
-  event = {
+  event1 = {
     id: 1,
     name: 'Angular Connect',
     date: '9/26/2036',
