fd52856af60aa01fbf6f65f3d33b2b5096de4122
blightfoot71 <brianlightfoot71@gmail.com>
2020-05-04 15:43:00 -0500

    Finished Section 8 Module 6

--- Angular/Angular-Fundementals/ng-fundamentals/src/app/app.module.ts
+++ Angular/Angular-Fundementals/ng-fundamentals/src/app/app.module.ts
@@ -25,6 +25,18 @@ import { Error404Component } from './errors/404.component';
     CreateEventComponent,
     Error404Component
   ],
+  providers: [
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this event, do you readlly want to cancel?')
+  return true
+}
