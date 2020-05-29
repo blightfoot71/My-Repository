5d9f1d34f6e799627e3ecf5afae3b752fa9f2ded
blightfoot71 <brianlightfoot71@gmail.com>
2020-05-29 15:16:32 -0500

    Section 2 Module 9

--- Angular/Angular-Fundementals/ng-fundamentals/src/app/app.module.ts
+++ Angular/Angular-Fundementals/ng-fundamentals/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { NavBarComponent } from './nav/navbar.component';
 import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-//import { SessionListComponent } from './events/event-details/session-list.component';
+import { CollapsibleWellComponent } from './common/collapsible-well.component';
 
 @NgModule({
   imports: [
@@ -34,7 +34,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CreateEventComponent,
     Error404Component,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    CollapsibleWellComponent
   ],
   providers: [
     {
