c10a4eefae7a4f80deeabb7a2f797bbd2cda1374
blightfoot71 <brianlightfoot71@gmail.com>
2020-08-29 15:50:05 -0500

    Module 11 section 5

--- Angular/Angular-Fundementals/ng-fundamentals/src/app/user/profile.component.ts
+++ Angular/Angular-Fundementals/ng-fundamentals/src/app/user/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Inject } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from './auth.service'
 import { Router } from '@angular/router'
+import { TOASTR_TOKEN, Toastr } from '../common/toastr.service'
 
 @Component({
   templateUrl: './profile.component.html',
@@ -17,7 +18,8 @@ export class ProfileComponent implements OnInit {
   private lastName: FormControl
 
 constructor (private authService: AuthService,
-             private router: Router) {}
+             private router: Router,
+             @Inject(TOASTR_TOKEN) private toastr: Toastr) {}
 
   ngOnInit() {
     this.firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
@@ -43,7 +45,8 @@ constructor (private authService: AuthService,
   saveProfile(formValues) {
     if(this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      this.router.navigate(['events'])
+      //this.router.navigate(['events'])
+      this.toastr.success('Profile Saved');
     }
   }
 }
