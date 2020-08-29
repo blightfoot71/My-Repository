88f5bcaf8dee34e770cfc029211e8bb7dcc1ce30
blightfoot71 <brianlightfoot71@gmail.com>
2020-08-29 16:14:42 -0500

    Revert "Module 11 section 5"
    This reverts commit c10a4eefae7a4f80deeabb7a2f797bbd2cda1374.

--- Angular/Angular-Fundementals/ng-fundamentals/src/app/user/profile.component.ts
+++ Angular/Angular-Fundementals/ng-fundamentals/src/app/user/profile.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from './auth.service'
 import { Router } from '@angular/router'
-import { TOASTR_TOKEN, Toastr } from '../common/toastr.service'
 
 @Component({
   templateUrl: './profile.component.html',
@@ -18,8 +17,7 @@ export class ProfileComponent implements OnInit {
   private lastName: FormControl
 
 constructor (private authService: AuthService,
-             private router: Router,
-             @Inject(TOASTR_TOKEN) private toastr: Toastr) {}
+             private router: Router) {}
 
   ngOnInit() {
     this.firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
@@ -45,8 +43,7 @@ constructor (private authService: AuthService,
   saveProfile(formValues) {
     if(this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      //this.router.navigate(['events'])
-      this.toastr.success('Profile Saved');
+      this.router.navigate(['events'])
     }
   }
 }
